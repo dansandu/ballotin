15716e551695fa49624757c5801928ff9ae93f7c
Daniel Sandu <3251086-dansandu@users.noreply.gitlab.com>
2019-04-21 20:34:19 +0300

    Add type traits

---
+++ sources/dansandu/ballotin/type_traits.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <type_traits>
+#include <variant>
+
+namespace dansandu::ballotin::type_traits {
+
+struct Uncopyable {
+    Uncopyable() = default;
+
+    Uncopyable(Uncopyable&&) = default;
+
+    Uncopyable(const Uncopyable&) = delete;
+
+    Uncopyable& operator=(Uncopyable&&) = default;
+
+    Uncopyable& operator=(const Uncopyable&) = delete;
+};
+
+struct Immovable {
+    Immovable() = default;
+
+    Immovable(Immovable&&) = delete;
+
+    Immovable& operator=(Immovable&&) = delete;
+};
+
+template<typename... Types>
+struct type_pack {
+    using as_variant_type = std::variant<Types...>;
+
+    template<typename T>
+    static constexpr auto contains = (... || std::is_same_v<T, Types>);
+};
+}
