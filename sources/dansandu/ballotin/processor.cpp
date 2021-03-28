fdc9843bc26df5d6ba30ab1f6c3397c7fcaf4f40
Daniel Sandu <49592096+dansandu@users.noreply.github.com>
2021-03-28 16:05:01 +0300

    Add processor

---
+++ sources/dansandu/ballotin/processor.cpp
@@ -0,0 +1,142 @@
+#include "dansandu/ballotin/processor.hpp"
+
+namespace dansandu::ballotin::processor
+{
+
+Processor::Processor(std::unique_ptr<IProcess> process) : process_{std::move(process)}
+{
+    if (process_)
+    {
+        thread_ = std::thread{&Processor::loop, this};
+    }
+}
+
+void Processor::resume()
+{
+    if (process_)
+    {
+        {
+            const auto lock = std::lock_guard{mutex_};
+            queue_.push_back(Signal::resume);
+        }
+        conditionVariable_.notify_one();
+    }
+}
+
+void Processor::pause()
+{
+    if (process_)
+    {
+        {
+            const auto lock = std::lock_guard{mutex_};
+            queue_.push_back(Signal::pause);
+        }
+        conditionVariable_.notify_one();
+    }
+}
+
+void Processor::reset()
+{
+    if (process_)
+    {
+        {
+            const auto lock = std::lock_guard{mutex_};
+            queue_.push_back(Signal::reset);
+        }
+        conditionVariable_.notify_one();
+    }
+}
+
+std::unique_ptr<IProcess> Processor::yield()
+{
+    if (process_)
+    {
+        {
+            const auto lock = std::lock_guard{mutex_};
+            queue_.push_back(Signal::yield);
+        }
+        conditionVariable_.notify_one();
+        thread_.join();
+        return std::move(process_);
+    }
+    return {};
+}
+
+std::unique_ptr<IProcess> Processor::finishAndYield()
+{
+    if (process_)
+    {
+        {
+            const auto lock = std::lock_guard{mutex_};
+            queue_.push_back(Signal::finishAndYield);
+        }
+        conditionVariable_.notify_one();
+        thread_.join();
+        return std::move(process_);
+    }
+    return {};
+}
+
+Processor::~Processor()
+{
+    yield();
+}
+
+void Processor::loop()
+{
+    auto paused = true;
+    auto done = false;
+    auto finishing = false;
+
+    while (!done | !finishing)
+    {
+        if (!finishing)
+        {
+            auto reset = false;
+
+            {
+                auto lock = std::unique_lock{mutex_};
+                conditionVariable_.wait(lock, [paused, this] { return !queue_.empty() | !paused; });
+
+                for (auto index = 0U; index < queue_.size(); ++index)
+                {
+                    switch (queue_[index])
+                    {
+                    case Signal::resume:
+                        paused = done;
+                        break;
+                    case Signal::pause:
+                        paused = true;
+                        break;
+                    case Signal::reset:
+                        paused = true;
+                        done = false;
+                        reset = true;
+                        break;
+                    case Signal::yield:
+                        return;
+                    case Signal::finishAndYield:
+                        paused = done;
+                        finishing = true;
+                        index = queue_.size();
+                        break;
+                    }
+                }
+
+                queue_.clear();
+            }
+
+            if (reset)
+            {
+                process_->reset();
+            }
+        }
+
+        if (!paused & !done)
+        {
+            paused = done = !process_->tick();
+        }
+    }
+}
+
+}
---
+++ sources/dansandu/ballotin/processor.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#include "dansandu/ballotin/type_traits.hpp"
+
+#include <condition_variable>
+#include <memory>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+namespace dansandu::ballotin::processor
+{
+
+class IProcess : private dansandu::ballotin::type_traits::Uncopyable, private dansandu::ballotin::type_traits::Immovable
+{
+public:
+    virtual ~IProcess()
+    {
+    }
+
+    virtual void reset() = 0;
+
+    virtual bool tick() = 0;
+};
+
+class Processor : private dansandu::ballotin::type_traits::Uncopyable,
+                  private dansandu::ballotin::type_traits::Immovable
+{
+public:
+    Processor(std::unique_ptr<IProcess> process);
+
+    ~Processor();
+
+    void resume();
+
+    void pause();
+
+    void reset();
+
+    std::unique_ptr<IProcess> yield();
+
+    std::unique_ptr<IProcess> finishAndYield();
+
+private:
+    void loop();
+
+    enum class Signal
+    {
+        resume,
+        pause,
+        reset,
+        yield,
+        finishAndYield
+    };
+
+    std::unique_ptr<IProcess> process_;
+    std::mutex mutex_;
+    std::condition_variable conditionVariable_;
+    std::vector<Signal> queue_;
+    std::thread thread_;
+};
+
+}
