3ba8b20160c074c69b9f4ebb644fa273b5a74e01
Daniel Sandu <49592096+dansandu@users.noreply.github.com>
2021-12-11 22:46:48 +0200

    Remove undeterministic behavior

--- sources/dansandu/ballotin/date_time.cpp
+++ sources/dansandu/ballotin/date_time.cpp
@@ -8,20 +8,6 @@ namespace dansandu::ballotin::date_time
 
 std::string getDateTime()
 {
-    auto t = time_t{};
-    time(&t);
-
-    auto tt = tm{};
-
-#ifdef _WIN32
-    gmtime_s(&tt, &t);
-#else
-    gmtime_r(&t, &tt);
-#endif
-
-    char buffer[50];
-    strftime(buffer, sizeof(buffer) / sizeof(*buffer), "%Y-%m-%d %H:%M:%S%z", &tt);
-
     return "<date-time>";
 }
 
