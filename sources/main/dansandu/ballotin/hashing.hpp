1f4fcff83f9e1662e288a8ea7dfe3e8c1f1d932d
Daniel Sandu <49592096+dansandu@users.noreply.github.com>
2025-08-10 13:22:43 +0300

    32-bits hash code

--- sources/main/dansandu/ballotin/hashing.hpp
+++ sources/main/dansandu/ballotin/hashing.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "dansandu/ballotin/binary.hpp"
+
 #include <cstdint>
 #include <string>
 #include <type_traits>
@@ -8,13 +10,60 @@ namespace dansandu::ballotin::hashing
 {
 
 template<typename T>
-uint64_t getHashCode(const T& value)
+uint32_t getHashCode32(const T& value)
 {
+    using dansandu::ballotin::binary::bitsPerByte;
+    using ValueType = std::decay_t<T>;
+
+    constexpr uint32_t offset = 0x811C9DC5u;
+    constexpr uint32_t prime = 0x01000193u;
+
+    uint32_t hashCode = offset;
+
+    if constexpr (std::is_arithmetic_v<ValueType>)
+    {
+        for (size_t index = 0; index < sizeof(value); ++index)
+        {
+            hashCode ^= (value >> (bitsPerByte * index)) & 0xFF;
+            hashCode *= prime;
+        }
+    }
+    else if constexpr (std::is_enum_v<ValueType>)
+    {
+        using UnderlyingType = std::underlying_type_t<ValueType>;
+
+        const auto actualValue = static_cast<UnderlyingType>(value);
+
+        for (size_t index = 0; index < sizeof(UnderlyingType); ++index)
+        {
+            hashCode ^= (actualValue >> (bitsPerByte * index)) & 0xFF;
+            hashCode *= prime;
+        }
+    }
+    else if constexpr (std::is_same_v<ValueType, std::string>)
+    {
+        for (const auto character : value)
+        {
+            hashCode ^= character;
+            hashCode *= prime;
+        }
+    }
+    else
+    {
+        static_assert("Type not supported");
+    }
+
+    return hashCode;
+}
+
+template<typename T>
+uint64_t getHashCode64(const T& value)
+{
+    using dansandu::ballotin::binary::bitsPerByte;
     using ValueType = std::decay_t<T>;
 
     constexpr uint64_t offset = 0xCBF29CE484222325ull;
     constexpr uint64_t prime = 0x00000100000001B3ull;
-    constexpr size_t bitsPerByte = 8;
 
     uint64_t hashCode = offset;
 
@@ -45,6 +94,10 @@ uint64_t getHashCode(const T& value)
             hashCode ^= character;
             hashCode *= prime;
         }
+    }
+    else
+    {
+        static_assert("Type not supported");
     }
 
     return hashCode;
