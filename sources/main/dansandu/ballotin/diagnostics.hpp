1056629b64a0d86e89e4ac914fd1a304fcb3c83a
Daniel Sandu <49592096+dansandu@users.noreply.github.com>
2026-02-14 18:18:56 +0200

    Add strongly typed function

---
+++ sources/main/dansandu/ballotin/diagnostics.hpp
@@ -0,0 +1,106 @@
+#pragma once
+
+namespace dansandu::ballotin::diagnostics
+{
+
+class TrackedObjectSession
+{
+public:
+    friend class TrackedObject;
+
+    TrackedObjectSession()
+        : createdInstances_{0},
+          copyConstructorCalls_{0},
+          moveConstructorCalls_{0},
+          copyAssignmentCalls_{0},
+          moveAssignmentCalls_{0},
+          destructorCalls_{0}
+    {
+    }
+
+    TrackedObjectSession(const TrackedObjectSession&) = delete;
+    TrackedObjectSession(TrackedObjectSession&&) noexcept = delete;
+    TrackedObjectSession& operator=(const TrackedObjectSession&) = delete;
+    TrackedObjectSession& operator=(TrackedObjectSession&&) noexcept = delete;
+
+    int getCreatedInstances() const
+    {
+        return createdInstances_;
+    }
+
+    int getCopyConstructorCalls() const
+    {
+        return copyConstructorCalls_;
+    }
+
+    int getMoveConstructorCalls() const
+    {
+        return moveConstructorCalls_;
+    }
+
+    int getCopyAssignmentCalls() const
+    {
+        return copyAssignmentCalls_;
+    }
+
+    int getMoveAssignmentCalls() const
+    {
+        return moveAssignmentCalls_;
+    }
+
+    int getDestructorCalls() const
+    {
+        return destructorCalls_;
+    }
+
+private:
+    int createdInstances_;
+    int copyConstructorCalls_;
+    int moveConstructorCalls_;
+    int copyAssignmentCalls_;
+    int moveAssignmentCalls_;
+    int destructorCalls_;
+};
+
+class TrackedObject
+{
+public:
+    explicit TrackedObject(TrackedObjectSession& session) : session_{session}
+    {
+        ++session_.createdInstances_;
+    }
+
+    TrackedObject(const TrackedObject& other) : session_{other.session_}
+    {
+        ++session_.createdInstances_;
+        ++session_.copyConstructorCalls_;
+    }
+
+    TrackedObject(TrackedObject&& other) noexcept : session_{other.session_}
+    {
+        ++session_.createdInstances_;
+        ++session_.moveConstructorCalls_;
+    }
+
+    TrackedObject& operator=(const TrackedObject& other)
+    {
+        ++session_.copyAssignmentCalls_;
+        return *this;
+    }
+
+    TrackedObject& operator=(TrackedObject&& other) noexcept
+    {
+        ++session_.moveAssignmentCalls_;
+        return *this;
+    }
+
+    ~TrackedObject() noexcept
+    {
+        ++session_.destructorCalls_;
+    }
+
+private:
+    TrackedObjectSession& session_;
+};
+
+}
--- sources/test/dansandu/ballotin/type_traits.test.cpp
+++ sources/test/dansandu/ballotin/type_traits.test.cpp
@@ -1,23 +1,67 @@
 #include "dansandu/ballotin/type_traits.hpp"
 #include "dansandu/radiance/radiance.hpp"
 
+using dansandu::ballotin::type_traits::Invokable;
 using dansandu::ballotin::type_traits::TypeDictionary;
 using dansandu::ballotin::type_traits::TypeEntry;
-using dansandu::ballotin::type_traits::TypePack;
+using dansandu::ballotin::type_traits::TypeList;
 
 TEST_CASE("type_traits")
 {
-    REQUIRE(TypePack<int, float, double>::contains<int>);
+    SECTION("type list")
+    {
+        REQUIRE(TypeList<int, float, double>::contains<int>);
 
-    REQUIRE(!TypePack<int, float, double>::contains<char>);
+        REQUIRE(!TypeList<int, float, double>::contains<char>);
+    }
 
-    using Dictionary = TypeDictionary<TypeEntry<int, unsigned>, TypeEntry<char, unsigned char>>;
+    SECTION("type dictionary")
+    {
+        using Dictionary = TypeDictionary<TypeEntry<int, unsigned>, TypeEntry<char, unsigned char>>;
 
-    REQUIRE(Dictionary::containsKey<int>);
+        REQUIRE(Dictionary::containsKey<int>);
 
-    REQUIRE(!Dictionary::containsKey<double>);
+        REQUIRE(!Dictionary::containsKey<double>);
 
-    REQUIRE(std::is_same_v<Dictionary::Get<double>, void>);
+        REQUIRE(std::is_same_v<Dictionary::Get<double>, void>);
 
-    REQUIRE(std::is_same_v<Dictionary::Get<int>, unsigned>);
+        REQUIRE(std::is_same_v<Dictionary::Get<int>, unsigned>);
+    }
+
+    SECTION("invokable void(int) const")
+    {
+        struct SomeInvokableType
+        {
+            void operator()(int) const
+            {
+            }
+        };
+
+        REQUIRE(Invokable<SomeInvokableType, void, int>);
+
+        REQUIRE(!Invokable<SomeInvokableType, int, int>);
+
+        REQUIRE(!Invokable<SomeInvokableType, void, int&>);
+
+        REQUIRE(!Invokable<SomeInvokableType, void, int&&>);
+
+        REQUIRE(!Invokable<SomeInvokableType, void>);
+    }
+
+    SECTION("invokable int()")
+    {
+        struct AnotherInvokableType
+        {
+            int operator()()
+            {
+                return 0;
+            }
+        };
+
+        REQUIRE(Invokable<AnotherInvokableType, int>);
+
+        REQUIRE(!Invokable<AnotherInvokableType, int, int>);
+
+        REQUIRE(!Invokable<AnotherInvokableType, void>);
+    }
 }
