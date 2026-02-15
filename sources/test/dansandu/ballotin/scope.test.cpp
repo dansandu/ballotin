4abaf86f23acc4df77408e4f820b6bad5b8d3df1
Daniel Sandu <49592096+dansandu@users.noreply.github.com>
2026-02-15 14:48:50 +0200

    Add SCOPE_SUCCESS and SCOPE_EXIT guards

--- sources/test/dansandu/ballotin/scope.test.cpp
+++ sources/test/dansandu/ballotin/scope.test.cpp
@@ -34,4 +34,64 @@ TEST_CASE("scope")
             REQUIRE(triggered);
         }
     }
+
+    SECTION("success guard")
+    {
+        SECTION("with success")
+        {
+            auto triggered = false;
+
+            {
+                SCOPE_SUCCESS([&] { triggered = true; });
+            }
+
+            REQUIRE(triggered);
+        }
+
+        SECTION("with failure")
+        {
+            auto triggered = false;
+
+            const auto failingFunction = [&]()
+            {
+                SCOPE_SUCCESS([&] { triggered = true; });
+
+                throw std::runtime_error("error");
+            };
+
+            REQUIRE_THROW(std::runtime_error, failingFunction());
+
+            REQUIRE(!triggered);
+        }
+    }
+
+    SECTION("exit guard")
+    {
+        SECTION("with success")
+        {
+            auto triggered = false;
+
+            {
+                SCOPE_EXIT([&] { triggered = true; });
+            }
+
+            REQUIRE(triggered);
+        }
+
+        SECTION("with failure")
+        {
+            auto triggered = false;
+
+            const auto failingFunction = [&]()
+            {
+                SCOPE_EXIT([&] { triggered = true; });
+
+                throw std::runtime_error("error");
+            };
+
+            REQUIRE_THROW(std::runtime_error, failingFunction());
+
+            REQUIRE(triggered);
+        }
+    }
 }
